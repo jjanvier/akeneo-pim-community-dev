5ad8c4c088f414a2b259b7601f19108cd63e353a
Julien Janvier (jjanvier) <j.janvier@gmail.com>
2017-02-27 16:53:18 +0100

    TO REMOVE: simple example of PQB search

---
+++ src/test-es-pqb.php
@@ -0,0 +1,22 @@
+<?php
+
+use Pim\Component\Catalog\Query\Filter\Operators;
+
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->boot();
+
+$c = $kernel->getContainer();
+
+$pqb = $c->get('pim_catalog.query.product_query_builder_factory')->create();
+$pqb->addFilter('family', Operators::IN_LIST, ['camcorders']);
+
+$products = $pqb->execute();
+
+echo sprintf("%d products found...\n", $products->count());
+foreach ($products as $product) {
+    echo $product->getIdentifier();
+    echo "\n";
+}
