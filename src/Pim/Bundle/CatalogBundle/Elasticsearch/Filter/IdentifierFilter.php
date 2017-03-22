7d5d08d5aeaf6b84a64341fc2b4f72c2d51f78f5
Samir Boulil <samir.boulil@akeneo.com>
2017-03-22 11:33:50 +0100

    TIP-706: IdentifierFilter can filter on attributes

--- src/Pim/Bundle/CatalogBundle/Elasticsearch/Filter/IdentifierFilter.php
+++ src/Pim/Bundle/CatalogBundle/Elasticsearch/Filter/IdentifierFilter.php
@@ -4,10 +4,11 @@ namespace Pim\Bundle\CatalogBundle\Elasticsearch\Filter;
 
 use Akeneo\Component\StorageUtils\Exception\InvalidPropertyTypeException;
 use Pim\Component\Catalog\Exception\InvalidOperatorException;
+use Pim\Component\Catalog\Model\AttributeInterface;
+use Pim\Component\Catalog\Query\Filter\AttributeFilterInterface;
 use Pim\Component\Catalog\Query\Filter\FieldFilterHelper;
 use Pim\Component\Catalog\Query\Filter\FieldFilterInterface;
 use Pim\Component\Catalog\Query\Filter\Operators;
-use Pim\Component\Catalog\Repository\AttributeRepositoryInterface;
 
 /**
  * Identifier filter for an Elasticsearch query
@@ -16,26 +17,26 @@ use Pim\Component\Catalog\Repository\AttributeRepositoryInterface;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class IdentifierFilter extends AbstractFieldFilter implements FieldFilterInterface
+class IdentifierFilter extends AbstractFieldFilter implements FieldFilterInterface, AttributeFilterInterface
 {
     const IDENTIFIER_KEY = 'identifier';
 
-    /** @var AttributeRepositoryInterface */
-    protected $attributeRepository;
+    /** @var string[] */
+    protected $supportedAttributeTypes;
 
     /**
-     * @param AttributeRepositoryInterface $attributeRepository
-     * @param array                        $supportedFields
-     * @param array                        $supportedOperators
+     * @param array $supportedFields
+     * @param array $supportedAttributeTypes
+     * @param array $supportedOperators
      */
     public function __construct(
-        AttributeRepositoryInterface $attributeRepository,
         array $supportedFields = [],
+        array $supportedAttributeTypes = [],
         array $supportedOperators = []
     ) {
         $this->supportedFields = $supportedFields;
         $this->supportedOperators = $supportedOperators;
-        $this->attributeRepository = $attributeRepository;
+        $this->supportedAttributeTypes = $supportedAttributeTypes;
     }
 
     /**
@@ -49,6 +50,74 @@ class IdentifierFilter extends AbstractFieldFilter implements FieldFilterInterfa
 
         $this->checkValue($field, $operator, $value);
 
+        $this->applyFilter($operator, $value);
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function addAttributeFilter(
+        AttributeInterface $attribute,
+        $operator,
+        $value,
+        $locale = null,
+        $scope = null,
+        $options = []
+    ) {
+        if (null === $this->searchQueryBuilder) {
+            throw new \LogicException('The search query builder is not initialized in the filter.');
+        }
+
+        $this->checkValue($attribute->getCode(), $operator, $value);
+        $this->applyFilter($operator, $value);
+
+        return $this;
+    }
+
+    /**
+     * Checks the identifier is a string or an array depending on the operator
+     *
+     * @param string $property
+     * @param string $operator
+     * @param mixed  $value
+     *
+     * @throws InvalidPropertyTypeException
+     */
+    protected function checkValue($property, $operator, $value)
+    {
+        if (Operators::IN_LIST === $operator || Operators::NOT_IN_LIST === $operator) {
+            FieldFilterHelper::checkArray($property, $value, self::class);
+        } else {
+            FieldFilterHelper::checkString($property, $value, self::class);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsAttribute(AttributeInterface $attribute)
+    {
+        return in_array($attribute->getType(), $this->supportedAttributeTypes);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAttributeTypes()
+    {
+        return $this->supportedAttributeTypes;
+    }
+
+    /**
+     * Apply the filtering conditions to the search query builder
+     *
+     * @param $operator
+     * @param $value
+     */
+    protected function applyFilter($operator, $value)
+    {
         switch ($operator) {
             case Operators::STARTS_WITH:
                 $clause = [
@@ -133,34 +202,6 @@ class IdentifierFilter extends AbstractFieldFilter implements FieldFilterInterfa
 
             default:
                 throw InvalidOperatorException::notSupported($operator, static::class);
-        }
-
-        return $this;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function supportsField($field)
-    {
-        return in_array($field, $this->supportedFields) || $field === $this->attributeRepository->getIdentifierCode();
-    }
-
-    /**
-     * Checks the identifier is a string or an array depending on the operator
-     *
-     * @param string $field
-     * @param string $operator
-     * @param mixed  $value
-     *
-     * @throws InvalidPropertyTypeException
-     */
-    protected function checkValue($field, $operator, $value)
-    {
-        if (Operators::IN_LIST === $operator || Operators::NOT_IN_LIST === $operator) {
-            FieldFilterHelper::checkArray($field, $value, self::class);
-        } else {
-            FieldFilterHelper::checkString($field, $value, self::class);
         }
     }
 }
