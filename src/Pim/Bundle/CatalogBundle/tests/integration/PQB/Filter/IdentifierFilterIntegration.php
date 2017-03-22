6aaf333aeb51e7d8ea5a1e51f97f31319e9ca6ea
Samir Boulil <samir.boulil@akeneo.com>
2017-03-22 15:22:57 +0100

    TIP-706: add more tests for attribute identifier

--- src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/IdentifierFilterIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/IdentifierFilterIntegration.php
@@ -36,6 +36,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
 
         $result = $this->execute([['identifier', Operators::STARTS_WITH, 'bA']]);
         $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::STARTS_WITH, 'ba']]);
+        $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::STARTS_WITH, 'bA']]);
+        $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
     }
 
     public function testOperatorContains()
@@ -45,6 +51,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
 
         $result = $this->execute([['identifier', Operators::CONTAINS, 'A']]);
         $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::CONTAINS, 'a']]);
+        $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::CONTAINS, 'A']]);
+        $this->assert($result, ['bar', 'baz', 'BARISTA', 'BAZAR']);
     }
 
     public function testOperatorNotContains()
@@ -53,6 +65,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
         $this->assert($result, ['foo']);
 
         $result = $this->execute([['identifier', Operators::DOES_NOT_CONTAIN, 'A']]);
+        $this->assert($result, ['foo']);
+
+        $result = $this->execute([['sku', Operators::DOES_NOT_CONTAIN, 'a']]);
+        $this->assert($result, ['foo']);
+
+        $result = $this->execute([['sku', Operators::DOES_NOT_CONTAIN, 'A']]);
         $this->assert($result, ['foo']);
     }
 
@@ -66,6 +84,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
 
         $result = $this->execute([['identifier', Operators::EQUALS, 'bazz']]);
         $this->assert($result, []);
+
+        $result = $this->execute([['sku', Operators::EQUALS, 'bazz']]);
+        $this->assert($result, []);
+
+        $result = $this->execute([['sku', Operators::EQUALS, 'bAz']]);
+        $this->assert($result, ['baz']);
     }
 
     public function testOperatorNotEquals()
@@ -78,6 +102,15 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
 
         $result = $this->execute([['identifier', Operators::NOT_EQUAL, 'bAz']]);
         $this->assert($result, ['foo', 'bar', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::NOT_EQUAL, 'bazz']]);
+        $this->assert($result, ['foo', 'bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::NOT_EQUAL, 'baz']]);
+        $this->assert($result, ['foo', 'bar', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::NOT_EQUAL, 'bAz']]);
+        $this->assert($result, ['foo', 'bar', 'BARISTA', 'BAZAR']);
     }
 
     public function testOperatorInList()
@@ -87,6 +120,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
 
         $result = $this->execute([['identifier', Operators::IN_LIST, ['bazz', 'FOOO']]]);
         $this->assert($result, []);
+
+        $result = $this->execute([['sku', Operators::IN_LIST, ['baz', 'FOO']]]);
+        $this->assert($result, ['foo', 'baz']);
+
+        $result = $this->execute([['sku', Operators::IN_LIST, ['BAZZ', 'FOOO']]]);
+        $this->assert($result, []);
     }
 
     public function testOperatorNotInList()
@@ -95,6 +134,12 @@ class IdentifierFilterIntegration extends AbstractFilterTestCase
         $this->assert($result, ['bar', 'BARISTA', 'BAZAR']);
 
         $result = $this->execute([['identifier', Operators::NOT_IN_LIST, ['bazz', 'FOOO']]]);
+        $this->assert($result, ['foo', 'bar', 'baz', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::NOT_IN_LIST, ['baz', 'FOO']]]);
+        $this->assert($result, ['bar', 'BARISTA', 'BAZAR']);
+
+        $result = $this->execute([['sku', Operators::NOT_IN_LIST, ['BAZZ', 'FOOO']]]);
         $this->assert($result, ['foo', 'bar', 'baz', 'BARISTA', 'BAZAR']);
     }
 
