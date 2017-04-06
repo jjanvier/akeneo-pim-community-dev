1644184e169c33ddbf82b18c3ed283279d531658
Samir Boulil <samir.boulil@akeneo.com>
2017-04-06 14:49:35 +0200

    TIP-706: Add variant group filter integrations

--- src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/GroupsFilterIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/GroupsFilterIntegration.php
@@ -6,6 +6,8 @@ use Pim\Bundle\CatalogBundle\tests\integration\PQB\AbstractProductQueryBuilderTe
 use Pim\Component\Catalog\Query\Filter\Operators;
 
 /**
+ * Integration tests of the PQB to test filters on groups and variant groups.
+ *
  * @author    Marie Bochu <marie.bochu@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
@@ -20,6 +22,11 @@ class GroupsFilterIntegration extends AbstractProductQueryBuilderTestCase
         parent::setUp();
 
         if (1 === self::$count || $this->getConfiguration()->isDatabasePurgedForEachTest()) {
+
+            $this->createProduct('foo', ['groups' => ['groupA', 'groupB', 'variantA', 'variantB']]);
+            $this->createProduct('bar', ['groups' => ['groupA', 'variantA']]);
+            $this->createProduct('baz', []);
+
             $group = $this->get('pim_catalog.factory.group')->create();
             $this->get('pim_catalog.updater.group')->update($group, [
                 'code' => 'groupC',
@@ -27,9 +34,12 @@ class GroupsFilterIntegration extends AbstractProductQueryBuilderTestCase
             ]);
             $this->get('pim_catalog.saver.group')->save($group);
 
-            $this->createProduct('foo', ['groups' => ['groupA', 'groupB']]);
-            $this->createProduct('bar', []);
-            $this->createProduct('baz', []);
+            $group = $this->get('pim_catalog.factory.group')->create();
+            $this->get('pim_catalog.updater.variant_group') ->update($group, [
+                'code' => 'variantC',
+                'type' => 'VARIANT'
+            ]);
+            $this->get('pim_catalog.saver.group')->save($group);
         }
     }
 
@@ -39,34 +49,64 @@ class GroupsFilterIntegration extends AbstractProductQueryBuilderTestCase
         $this->assert($result, []);
 
         $result = $this->executeFilter([['groups', Operators::IN_LIST, ['groupB', 'groupA']]]);
+        $this->assert($result, ['foo', 'bar']);
+
+        $result = $this->executeFilter([['groups', Operators::IN_LIST, ['groupB']]]);
         $this->assert($result, ['foo']);
 
-        $result = $this->executeFilter([['groups', Operators::IN_LIST, ['groupA']]]);
+        $result = $this->executeFilter([['groups', Operators::IN_LIST, ['variantC']]]);
+        $this->assert($result, []);
+
+        $result = $this->executeFilter([['groups', Operators::IN_LIST, ['variantB', 'variantA']]]);
+        $this->assert($result, ['foo', 'bar']);
+
+        $result = $this->executeFilter([['groups', Operators::IN_LIST, ['variantB']]]);
         $this->assert($result, ['foo']);
     }
 
     public function testOperatorNotIn()
     {
         $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['groupA']]]);
-        $this->assert($result, ['bar', 'baz']);
+        $this->assert($result, ['baz']);
 
         $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['groupB']]]);
         $this->assert($result, ['bar', 'baz']);
 
         $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['groupC']]]);
         $this->assert($result, ['bar', 'baz', 'foo']);
+
+        $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['groupA', 'groupB']]]);
+        $this->assert($result, ['baz']);
+
+        $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['variantA']]]);
+        $this->assert($result, ['baz']);
+
+        $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['variantB']]]);
+        $this->assert($result, ['bar', 'baz']);
+
+        $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['variantC']]]);
+        $this->assert($result, ['bar', 'baz', 'foo']);
+
+        $result = $this->executeFilter([['groups', Operators::NOT_IN_LIST, ['variantA', 'variantB']]]);
+        $this->assert($result, ['baz']);
     }
 
     public function testOperatorEmpty()
     {
         $result = $this->executeFilter([['groups', Operators::IS_EMPTY, '']]);
-        $this->assert($result, ['bar', 'baz']);
+        $this->assert($result, ['baz']);
+
+        $result = $this->executeFilter([['groups', Operators::IS_EMPTY, null]]);
+        $this->assert($result, ['baz']);
     }
 
     public function testOperatorNotEmpty()
     {
         $result = $this->executeFilter([['groups', Operators::IS_NOT_EMPTY, '']]);
-        $this->assert($result, ['foo']);
+        $this->assert($result, ['foo', 'bar']);
+
+        $result = $this->executeFilter([['groups', Operators::IS_NOT_EMPTY, null]]);
+        $this->assert($result, ['foo', 'bar']);
     }
 
     /**
@@ -82,8 +122,17 @@ class GroupsFilterIntegration extends AbstractProductQueryBuilderTestCase
      * @expectedException \Pim\Component\Catalog\Exception\UnsupportedFilterException
      * @expectedExceptionMessage Filter on property "groups" is not supported or does not support operator "BETWEEN"
      */
-    public function testErrorOperatorNotSupported()
+    public function testErrorOperatorNotSupportedForGroups()
     {
         $this->executeFilter([['groups', Operators::BETWEEN, 'groupB']]);
+    }
+
+    /**
+     * @expectedException \Pim\Component\Catalog\Exception\UnsupportedFilterException
+     * @expectedExceptionMessage Filter on property "groups" is not supported or does not support operator "BETWEEN"
+     */
+    public function testErrorOperatorNotSupportedForVariantGroups()
+    {
+        $this->executeFilter([['groups', Operators::BETWEEN, 'variantB']]);
     }
 }
--- src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/IdentifierFilterIntegration.php
+++ src/Pim/Bundle/CatalogBundle/tests/integration/PQB/Filter/IdentifierFilterIntegration.php
@@ -6,7 +6,7 @@ use Pim\Bundle\CatalogBundle\tests\integration\PQB\AbstractProductQueryBuilderTe
 use Pim\Component\Catalog\Query\Filter\Operators;
 
 /**
- * @author    Samir Boulil <samir.boulil@gmail.com>
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
