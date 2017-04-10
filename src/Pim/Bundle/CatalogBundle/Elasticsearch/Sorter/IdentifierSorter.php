f4f4c65b2ea3fd772e9dbeacfbc5ef33204d6f36
Samir Boulil <samir.boulil@akeneo.com>
2017-04-10 17:53:29 +0200

    fix comments

--- src/Pim/Bundle/CatalogBundle/Elasticsearch/Sorter/IdentifierSorter.php
+++ src/Pim/Bundle/CatalogBundle/Elasticsearch/Sorter/IdentifierSorter.php
@@ -2,12 +2,8 @@
 
 namespace Pim\Bundle\CatalogBundle\Elasticsearch\Sorter;
 
-use Pim\Bundle\CatalogBundle\Elasticsearch\SearchQueryBuilder;
-use Pim\Component\Catalog\Exception\InvalidDirectionException;
 use Pim\Component\Catalog\Model\AttributeInterface;
 use Pim\Component\Catalog\Query\Sorter\AttributeSorterInterface;
-use Pim\Component\Catalog\Query\Sorter\Directions;
-use Pim\Component\Catalog\Query\Sorter\FieldSorterInterface;
 
 /**
  * Identifier sorter for an Elasticsearch query.
@@ -19,14 +15,8 @@ use Pim\Component\Catalog\Query\Sorter\FieldSorterInterface;
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class IdentifierSorter implements FieldSorterInterface, AttributeSorterInterface
+class IdentifierSorter extends BaseFieldSorter implements AttributeSorterInterface
 {
-    /** @var SearchQueryBuilder */
-    protected $searchQueryBuilder;
-
-    /** @var array */
-    protected $supportedFields;
-
     /** @var array */
     protected $supportedAttributes;
 
@@ -38,56 +28,6 @@ class IdentifierSorter implements FieldSorterInterface, AttributeSorterInterface
     {
         $this->supportedFields = $supportedFields;
         $this->supportedAttributes = $supportedAttributes;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function setQueryBuilder($searchQueryBuilder)
-    {
-        if (!$searchQueryBuilder instanceof SearchQueryBuilder) {
-            throw new \InvalidArgumentException(
-                sprintf('Query builder should be an instance of "%s"', SearchQueryBuilder::class)
-            );
-        }
-
-        $this->searchQueryBuilder = $searchQueryBuilder;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function addFieldSorter($field, $direction, $locale = null, $channel = null)
-    {
-        if (null === $this->searchQueryBuilder) {
-            throw new \LogicException('The search query builder is not initialized in the sorter.');
-        }
-
-        switch ($direction) {
-            case Directions::ASCENDING:
-                $sortClause = [
-                    $field => [
-                        'order'   => 'ASC',
-                        'missing' => '_last',
-                    ],
-                ];
-                $this->searchQueryBuilder->addSort($sortClause);
-
-                break;
-            case Directions::DESCENDING:
-                $sortClause = [
-                    $field => [
-                        'order'   => 'DESC',
-                        'missing' => '_last',
-                    ],
-                ];
-
-                $this->searchQueryBuilder->addSort($sortClause);
-
-                break;
-            default:
-                throw InvalidDirectionException::notSupported($direction, static::class);
-        }
     }
 
     /**
